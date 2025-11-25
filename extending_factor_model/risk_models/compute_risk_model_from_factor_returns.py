597f95c1f7c49cb6fd31df0cb4c147fe80ee0c89
Alexandros Tzikas <33734693+alextzik@users.noreply.github.com>
2025-11-25 15:04:16 -0800

    EDIT: Reorg

---
+++ extending_factor_model/risk_models/compute_risk_model_from_factor_returns.py
@@ -0,0 +1,9 @@
+"""
+Module for computing a basic risk model from factor returns.
+
+We compute a risk model F G F^T + D, where F is the factor loadings matrix, G is the factor covariance matrix,
+and D is the specific risk diagonal matrix, using regression techniques.
+
+
+"""
+
